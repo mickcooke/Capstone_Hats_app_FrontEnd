24610e1715a956c55ec230f41a998b7e231cd193
Iain <iain.mccall.95@gmail.com>
2023-09-02 19:36:08 +0100

    css

--- src/components/MoneyEarnedChart.js
+++ src/components/MoneyEarnedChart.js
@@ -1,6 +1,7 @@
 import React from 'react'
-// import Chart from "chart.js/auto";
+import Chart from "chart.js/auto";
 import { Doughnut } from "react-chartjs-2";
+import './MoneyContainer.css'
 
 const MoneyEarnedChart = ({owedAndPaidData}) => {
     
@@ -25,15 +26,24 @@ const data = {
 
   return (
     <div>
-        <Doughnut data={data}/>
-        <div className='doughnut-text'>
-            <p>You have earned </p>
-            <h1>£{owedAndPaidData[0] + owedAndPaidData[1]}</h1>
-            <p>You are owed </p>
-            <h2>£{owedAndPaidData[0]}</h2>
+        <div className='doughnut-container'>
+            
+            <Doughnut data={data} />
+
+            <div className='doughnut-middle-text'>
+                <p>You have earned </p>
+                <h1>£{owedAndPaidData[0] + owedAndPaidData[1]}</h1>
+                <p>You are owed </p>
+                <h2>£{owedAndPaidData[0]}</h2>
+            </div>
+
+            <div className='doughnut-footer-text'>
+                <p>You have been paid</p>
+                <h2>£{owedAndPaidData[1]}</h2>
+            </div>
+
         </div>
-        <p>You have been paid</p>
-        <h2>£{owedAndPaidData[1]}</h2>
+        
     </div>
   )
 }
