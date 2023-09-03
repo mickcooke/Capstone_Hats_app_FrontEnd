d4b4f5fec45b1416fd374d7cc078f1d5bb843d81
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-09-03 12:26:38 +0100

    added back button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+
 
 const Header = ({text}) => {
+
+const navigate = useNavigate();
+const goBack = () => {
+  navigate(-1);
+}
+
   return (
       <>
+      <div className='back-button'>
+      <img src={require("../assets/images/back-arrow.png")} className="back-arrow-icon" onClick={goBack}/>
+      </div>
       <div className='App'>
+        
       <p className='header-text'>{text}</p>
       </div>
       <hr/>
