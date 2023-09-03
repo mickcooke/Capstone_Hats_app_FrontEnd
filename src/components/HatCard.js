43034d840be683a067d4bd1193eb47640603b97d
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-09-03 18:07:55 +0100

    added ongoing job and unpaid job cards to hat card

--- src/components/HatCard.js
+++ src/components/HatCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const HatCard = ({ hat }) => {
+  
   if (!hat) {
     return <p>Loading...</p>;
   }
@@ -9,26 +10,115 @@ const HatCard = ({ hat }) => {
   const url = `/clients/${hat.id}`;
   const editUrl = `/hats/edit/${hat.id}`;
 
+  let numberOfOngoingJobs = 0;
+  let numberOfUnpaidJobs = 0;
+
+  const calculateOngoingJobs = () => {
+    const clients = hat.clients;
+    const allHatJobs = [];
+
+    for (const client of clients) {
+      for (const job of client.jobs) {
+        allHatJobs.push(job);
+      }
+    }
+
+    for (const job of allHatJobs) {
+      if (job.completed === false) {
+        numberOfOngoingJobs += 1;
+      }
+    }
+  };
+
+  calculateOngoingJobs();
+
+  const calculateUnpaidJobs = () => {
+    const clients = hat.clients;
+    const allHatJobs = [];
+
+    for (const client of clients) {
+      for (const job of client.jobs) {
+        allHatJobs.push(job);
+      }
+    }
+
+    for (const job of allHatJobs) {
+      if (job.completed === true && job.paid === false) {
+        numberOfUnpaidJobs += 1;
+      }
+    }
+  };
+
+  calculateUnpaidJobs();
+
+  const noOngoingJobs = () => {
+    return numberOfOngoingJobs === 0;
+  };
+
+  const ongoingJob = () => {
+    return numberOfOngoingJobs === 1;
+  };
+
+  const noUnpaidJobs = () => {
+    return numberOfUnpaidJobs === 0;
+  };
+
+  const unpaidJob = () => {
+    return numberOfUnpaidJobs === 1;
+  };
+
   return (
     <>
       <div className="hat-card-wrap">
         <Link to={url}>
           <div className="hat-card">
-            <div>
+            <div className="card-image-box">
               <img
                 src={require("../assets/images/trilby.png")}
                 className="hat-image"
+                alt="hat"
               />
             </div>
-            <div>
+            <div className="card-text-box">
               <p className="hat-name-text">{hat.name}</p>
 
+              {noOngoingJobs() ? (
+                <p></p>
+              ) : ongoingJob() ? (
+                <div className="ongoing-job-card">
+                  <p className="ongoing-job-text">
+                    {numberOfOngoingJobs} ongoing job
+                  </p>
+                </div>
+              ) : (
+                <div className="ongoing-job-card">
+                  <p className="ongoing-job-text">
+                    {numberOfOngoingJobs} ongoing jobs
+                  </p>
+                </div>
+              )}
+
+              {noUnpaidJobs() ? (
+                <p></p>
+              ) : unpaidJob() ? (
+                <div className="unpaid-job-card">
+                  <p className="ongoing-job-text">
+                    {numberOfUnpaidJobs} unpaid job
+                  </p>
+                </div>
+              ) : (
+                <div className="unpaid-job-card">
+                  <p className="ongoing-job-text">
+                    {numberOfUnpaidJobs} unpaid jobs
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         </Link>
         <Link to={editUrl}>
           <div className="card-edit-button">edit</div>
-          </Link>
+        </Link>
       </div>
     </>
   );
---
+++ src/styling_codes/HatIcons.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HatIcons = () => {
+  const hatIcons = ["trilby.png", "muskateer.png"];
+
+  return hatIcons;
+};
+
+export default HatIcons;
