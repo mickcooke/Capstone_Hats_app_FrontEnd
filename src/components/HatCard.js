287eca28b038101246d44501b15b9dd6620541fa
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-09-03 13:34:13 +0100

    added edit button

--- src/components/HatCard.js
+++ src/components/HatCard.js
@@ -1,34 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const HatCard = ({hat}) => {
-  if(!hat){
-    return(
-      <p>Loading...</p>
-    )
+const HatCard = ({ hat }) => {
+  if (!hat) {
+    return <p>Loading...</p>;
   }
 
   const url = `/clients/${hat.id}`;
   const editUrl = `/hats/edit/${hat.id}`;
 
   return (
-    <div className="hat-box">
+    <>
+      <div className="hat-card-wrap">
+        <Link to={url}>
+          <div className="hat-card">
+            <div>
+              <img
+                src={require("../assets/images/trilby.png")}
+                className="hat-image"
+              />
+            </div>
+            <div>
+              <p className="hat-name-text">{hat.name}</p>
 
-<div className="leftie">
-<img src={require("../assets/images/trilby.png")} className="hat-icon"/>
-
-</div>
-<div>
-
-      <Link to={url}>
-        <p>{hat.name}</p>
-      </Link>
-      <Link to={editUrl}>
-        <p>Edit Hat</p>
-      </Link>
-
+            </div>
+          </div>
+        </Link>
+        <Link to={editUrl}>
+          <div className="card-edit-button">edit</div>
+          </Link>
       </div>
-    </div>
+    </>
   );
 };
 
