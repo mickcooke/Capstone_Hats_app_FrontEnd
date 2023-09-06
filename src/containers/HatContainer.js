51df49499f0bb5cfa143047156fcadc0e3be54cb
mickcooke <128602534+mickcooke@users.noreply.github.com>
2023-09-06 22:33:29 +0100

    added sfx

--- src/containers/HatContainer.js
+++ src/containers/HatContainer.js
@@ -10,10 +10,15 @@ import ClientList from "../components/ClientList.js";
 import Header from "../components/Header.js";
 import HatForm from "../components/HatForm.js";
 import HatUpdateForm from "../components/HatUpdateForm.js";
+import useSound from 'use-sound';
+import HatSting from '../assets/sounds/Hats_Sting_Vintage.mp3'
 
 const HatContainer = ({ hats, jobs, clients, hatIcons, hatCardColours, user }) => {
   const text = "Your Hats";
   const newHatText = "Add a hat";
+
+  const [play] = useSound(HatSting, {volume: 0.2})
+    play();
 
 if(hats.length === 0) return (
 <>
