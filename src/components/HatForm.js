93574c88bb3c3acd02a5d0631d04cf40c857ba09
ablaney88 <132158397+ablaney88@users.noreply.github.com>
2023-09-06 13:10:51 +0100

    commit

--- src/components/HatForm.js
+++ src/components/HatForm.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import HatIconsNew from '../styling_codes/HatIconsNew'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-import HatIconsNew from '../styling_codes/HatIconsNew';
-import HatIcons from '../styling_codes/HatIcons';
 
 
 const HatForm = () => {
@@ -22,69 +20,21 @@ const HatForm = () => {
 
 console.log(HatIconsNew.mortar_board)
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    var formData = new FormData(e.target);
-
-    const form_values = Object.fromEntries(formData);
-    console.log("qq", form_values);
-  
-
 
   return (
     <div>
-      <form onSubmit={submitHandler}>
-
+      <form>
         <input type="text" placeholder="Hat Name" name="name" onChange={ handleChange }/>
-
+        
         {/* <select name="hat">
           <option><img src={HatIconsNew.mortar_board}></img></option>
           <option><img src={HatIconsNew.mortar_board}></img></option>
         </select> */}
-
-        <div>
-            <input
-              type="radio"
-              name="radio-image"
-              id="radio-form-image1"
-              defaultValue={"react"}
-            />
-            <label htmlFor="radio-form-image1">
-              <img
-                alt="radio-image-select-1"
-                layout="responsive"
-                width={"40px"}
-                height={"40px"}
-                src={mario}
-              />
-            </label>
-          </div>
-            <br />
-          <div>
-            <input
-              type="radio"
-              name="radio-image"
-              id="Radioform-image2"
-              defaultValue={"django"}
-            />
-            <label htmlFor="Radioform-image2">
-              <img
-                alt="image-select-2"
-                width={"30"}
-                height={"30"}
-                src={"/django.svg"}
-              />
-            </label>
-          </div>
-          <br />
-
-        
                 
                 <button type="submit" >Save</button>
       </form>
     </div>
   )
-}
 }
 
 export default HatForm
